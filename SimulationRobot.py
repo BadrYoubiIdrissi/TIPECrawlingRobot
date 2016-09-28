59595c50dd741bf20455245c6bfe0cfb586e6684
BadrYoubiIdrissi <badryoubiidrissi@gmail.com>
2016-09-28 11:52:07 +0200

    Update SimulationRobot.py

--- SimulationRobot.py
+++ SimulationRobot.py
@@ -4,7 +4,7 @@ from pymunk import pygame_util
 
 class Box(pygame.sprite.Sprite):
     
-    def __init__(self, width, height, position, mass = 10):
+    def __init__(self, width, height, position, mass = 1):
         pygame.sprite.Sprite.__init__(self)
         self.width = width
         self.height = height
@@ -18,28 +18,11 @@ class Box(pygame.sprite.Sprite):
         x,y = position
         self.body.position = (x+width/2,y+height/2)
         self.shape = pymunk.Poly.create_box(self.body,(width,height))
-
-    
+   
 def toPyGamePos(pos):
     return pos[0],600-pos[1]
-#class Arm(pygame.sprite.Sprite):
-#    
-#    def __init__(self, position, length, mass = 1):
-#        pygame.sprite.Sprite.__init__(self)
-#        self.length = length
-#        self.mass = mass
-##        self.moment = pymunk.moment_for_circle(self.mass, 0, 20)
-##        self.body = pymunk.Body(self.mass,self.moment)
-##        self.body.position = position
-##        self.shape = pymunk.Circle(self.body,20)
-#        x,y = position
-#        self.vertices = [(x,y),
-#                         (x+length,y),
-#                         (x,y+5),
-#                         (x+length,y+5)]
-#        self.moment = pymunk.moment_for_poly(mass, self.vertices)
-#        self.body = pymunk.Body(mass,self.moment)
-#        self.shape = pymunk.Poly(self.body, self.vertices)
+def add(c1,c2):
+    return (c1[0]+c2[0],c1[1]+c2[1])
         
 pygame.init()
 screen = pygame.display.set_mode((860,600), DOUBLEBUF)
@@ -54,26 +37,30 @@ space.damping = 0.9
 
 hauteur = 0
 
-box1 = Box(100,50, (0,hauteur),100000)
-box1.body.friction = 10000000
+box1 = Box(100,50, (300,200))
+box1.body.friction = 0.4
 
-j1 = Box(100,10,(120,hauteur+45))
+j1 = Box(100,10,add(box1.body.position,(70,20)),0.1)
 
-#j2 = Box(100,10,(240,hauteur+45))
+j2 = Box(100,10,add(box1.body.position,(190,20)),0.1)
+j2.body.friction = 1.05
 
-#constr = pymunk.constraint.PivotJoint(j1.body,j2.body,(230,hauteur+50))
-constr2 = pymunk.constraint.PivotJoint(box1.body, j1.body, (110,hauteur + 50))
 
-space.add(box1.body,box1.shape, j1.body, j1.shape, constr2)
+
+constr = pymunk.constraint.PivotJoint(j1.body,j2.body,add(box1.body.position,(180,25)))
+constr2 = pymunk.constraint.PivotJoint(box1.body, j1.body, add(box1.body.position,(60,25)))
+
+space.add(box1.body,box1.shape, j1.body, j1.shape, j2.shape, j2.body, constr2,constr)
 
 floor = pymunk.Body(body_type = pymunk.Body.STATIC)
+floor.friction = 1.0
 floor.position = (0,0)
 floorshape = pymunk.Segment(floor, (-1000, -5), (1000, -5), 5)
 
 space.add(floorshape)
 FPS = 60
-i = 200
-j = 200
+i = 0
+j = 0
 
 while True:
     screen.fill((255,255,255))
@@ -85,9 +72,9 @@ while True:
 #            i=j=0
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
-        i+= 100
+        i+= 10
     if keys[pygame.K_d]:
-        i-= 100
+        i-= 10
     if keys[pygame.K_w]:
         j+= 10
     if keys[pygame.K_s]:
@@ -101,14 +88,31 @@ while True:
     screen.blit(text, textpos)
             
     force = 0,i
-#    force2 = j*math.sin(j2.body.angle),-j*math.cos(j2.body.angle)
-    j1.body.apply_force_at_local_point((-40,0), force)
+    locPointOfApp = (40,0)
+    force2 = 0,j
     
-#    j2.body.apply_force_at_local_point((40,0), force2)
-#    j2.body.apply_force_at_local_point((0,0), (0,j2.mass*(900)))
-    pygame.draw.polygon(screen,(0,0,0),[toPyGamePos(j1.body.local_to_world((-40,0))),toPyGamePos(j1.body.local_to_world((-40,i/600)))],4)
-    space.step(1/FPS)
+    
+    pointOfApplication = j1.body.local_to_world(locPointOfApp)
+    pointOfApplication2 = j2.body.local_to_world(locPointOfApp)
+    reperex = toPyGamePos(j1.body.local_to_world((50,0)))
+    reperey = toPyGamePos(j1.body.local_to_world((0,50)))
+    origin = toPyGamePos(j1.body.local_to_world((0,0)))
+    
+    j1.body.apply_force_at_local_point(locPointOfApp, (0,0))
+    
+    j2.body.apply_force_at_local_point(locPointOfApp, (0,0))
+    
+    pygame.draw.polygon(screen,(0,0,0),[toPyGamePos(pointOfApplication),toPyGamePos(j1.body.local_to_world((locPointOfApp[0],i)))],4)
+    pygame.draw.polygon(screen,(0,0,0),[toPyGamePos(pointOfApplication2),toPyGamePos(j2.body.local_to_world((locPointOfApp[0],j)))],4)
+    
     space.debug_draw(drawopt)
+    
+    pygame.draw.polygon(screen,(0,0,255),[toPyGamePos(j1.body.position),toPyGamePos(add(j1.body.position,j1.body.force))],4)
+    pygame.draw.polygon(screen,(0,0,255),[toPyGamePos(j2.body.position),toPyGamePos(add(j2.body.position,j2.body.force))],4)
+    pygame.draw.polygon(screen,(0,0,255),[toPyGamePos(box1.body.position),toPyGamePos(add(box1.body.position,box1.body.force))],4)
+
+    pygame.draw.polygon(screen,(255,0,0),[origin, reperex], 3)
+    pygame.draw.polygon(screen,(0,255,0),[origin, reperey], 3)
     pygame.display.flip()
     clock.tick(FPS)
-    
+    space.step(1/FPS)
